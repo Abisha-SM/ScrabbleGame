7edf235c3d5927ac6075477347dab319b4acd3c1
Abisha-SM <abisha1720997@gmail.com>
2024-12-09 13:02:05 +0530

    -Added Gameplay functionality

---
+++ Assets/GameFlow Script/UISlot.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using System.Collections;
+
+public class UISlot : MonoBehaviour
+{
+
+    public GameObject UITile;
+
+    void Start()
+    {
+        UITile.GetComponent<UITile>().UISlot = gameObject;
+    }
+}
